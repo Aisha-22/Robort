27e292e300d815ab3f551f961913e4109b3b212f
Fikasentani <mpilo.dlamini@yahoo.com>
2020-11-04 21:15:55 +0200

    refactor

---
+++ src/com/company/Bus.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Bus {
+
+    int numberOfPeopleInTable = 10;
+   public void orderAlcohol(String alcoholType) {
+    System.out.println("give me a "  + alcoholType);
+   }
+
+   public void print() {
+       for (int i = 0; i < 14; i++) {
+       System.out.print("sdffhdfdfvbhdf");
+
+       }
+   }
+}
