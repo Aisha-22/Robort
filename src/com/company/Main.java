0df259744a0c05ffc92e7f7912008cd09af0c87a
Aishia <51677946+Aisha-22@users.noreply.github.com>
2020-11-04 08:55:17 +0200

    Robort with angela

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import  com.company.ParametersandArguments;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+      ParametersandArguments  pa = new ParametersandArguments();
+
+      pa.getMilk(3);
+    }
+}
---
+++ src/com/company/ParametersandArguments.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class ParametersandArguments {
+
+    public void getMilk(int money) {
+        System.out.println("leaveHouse");
+        System.out.println("moveRight");
+        System.out.println("moveRight");
+        System.out.println("moveUp");
+        System.out.println("moveUp");
+        System.out.println("moveUp");
+        System.out.println("moveUp");
+        System.out.println("moveRight");
+        System.out.println("moveRight");
+
+        int numberOfBottles = money * 15;
+
+        System.out.println("buy" + numberOfBottles + " bottles of Milk");
+        System.out.println("moveLeft");
+        System.out.println("moveLeft");
+        System.out.println("moveDown");
+        System.out.println("moveDown");
+        System.out.println("moveDown");
+        System.out.println("moveDown");
+        System.out.println("moveLeft");
+        System.out.println("moveLeft");
+        System.out.println("enterHouse");
+
+        return ;
+    }
+}
